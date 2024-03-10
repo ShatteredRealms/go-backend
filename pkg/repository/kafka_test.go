5a47369d4b8c05eb4f23a53e6b7f9cb7e2aaebdd
Wil Simpson <wil@forever.dev>
2024-03-10 15:46:42 -0400

    Readded kafka test

--- pkg/repository/kafka_test.go
+++ pkg/repository/kafka_test.go
@@ -1,26 +1,33 @@
 package repository_test
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo/v2"
-	// . "github.com/onsi/gomega"
-	//
-	// "github.com/ShatteredRealms/go-backend/pkg/config"
-	// "github.com/ShatteredRealms/go-backend/pkg/repository"
-	// testdb "github.com/ShatteredRealms/go-backend/test/db"
+	. "github.com/onsi/gomega"
+	"github.com/segmentio/kafka-go"
+
+	"github.com/ShatteredRealms/go-backend/pkg/config"
+	"github.com/ShatteredRealms/go-backend/pkg/repository"
+	testdb "github.com/ShatteredRealms/go-backend/test/db"
 )
 
 var _ = Describe("Kafka repository", func() {
 	Describe("ConnectKafka", func() {
 		Context("valid input", func() {
 			It("should work", func() {
-				// cleanupFunc, port := testdb.SetupKafkaWithDocker()
-				// defer cleanupFunc()
-				// kafka, err := repository.ConnectKafka(config.ServerAddress{
-				// 	Port: port,
-				// 	Host: "localhost",
-				// })
-				// Expect(err).NotTo(HaveOccurred())
-				// Expect(kafka).NotTo(BeNil())
+				cleanupFunc, port := testdb.SetupKafkaWithDocker()
+				var kafka *kafka.Conn
+				var err error
+				defer cleanupFunc()
+				Eventually(func(g Gomega) error {
+					kafka, err = repository.ConnectKafka(config.ServerAddress{
+						Port: port,
+						Host: "localhost",
+					})
+					return err
+				}).Within(time.Minute).Should(Succeed())
+				Expect(kafka).NotTo(BeNil())
 			})
 		})
 	})
