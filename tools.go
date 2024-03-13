ccfadb8dfd732c009c771634a3e3cd8da3ec11d7
Wil Simpson <wil@forever.dev>
2024-03-13 18:53:09 -0400

    Fixed ginkgo from not installing

--- tools.go
+++ tools.go
@@ -7,7 +7,7 @@ import (
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
 	_ "github.com/mitranim/gow@latest"
-	_ "github.com/onsi/ginkgo/v2"
+	_ "github.com/onsi/ginkgo/v2/ginkgo"
 	_ "go.uber.org/mock/mockgen"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
